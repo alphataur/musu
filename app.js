3b958b8448810f9d72e93eca1047995680533472
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-06-27 18:19:09 +0530

    updated readme

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require("body-parser")
 const APIRouter = require("./routes/index").APIRouter
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use("/api", APIRouter)
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+
 
 app.listen(3000, ()=>{
   console.log("server running at 3000!")
